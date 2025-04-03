cc14933d623f9fa6490a7ba9c0ee84e17457fad5
Oleksandra Kryvenko <alexxa.wrk@gmail.com>
2025-04-03 14:43:55 +0300

    correct typos with width

--- src/app/pages/todo-page/todo-page.component.ts
+++ src/app/pages/todo-page/todo-page.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 import { TodoBodyComponent } from '../../components/todo-body/todo-body.component';
 import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
   selector: 'app-todo-page',
   standalone: true,
-  imports: [SidebarComponent, TodoBodyComponent, HeaderComponent],
+  imports: [TodoBodyComponent, HeaderComponent],
   templateUrl: './todo-page.component.html',
   styleUrl: './todo-page.component.css',
 })
